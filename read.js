faed38b38aa50c31d9b3c1cf2ea4fd095af0c8a8
swathy <swathy.e30@wipro.com>
2016-09-01 09:16:27 +0530

    node

---
+++ read.js
@@ -0,0 +1,30 @@
+var fs=require('fs');
+var d;
+var content=[];
+var key=[];
+var last=[];
+fs.readFile("datafile_13.csv",function(err,data)
+{
+  console.log(data.toString());//console.log(err.stack);
+  d=data.toString();
+  content=d.split("\n");//console.log(content[0]);
+  key=content[0].split(",");//console.log(key[1]);
+  for(var i=1;i<=2;i++)
+  {
+    var obj={};
+    var value=content[i].split(",");
+    for(var p=0;p<2;p++)
+    {
+      obj[key[p]]=value[p];
+    }
+    console.log(JSON.stringify(obj));
+  }//  console.log(last);
+})
+//console.log(content[0]);
+// var p=0;
+// obj[key[p]]=value[p];
+// p++;
+// obj[key[p]]=value[p];
+// p++;
+// obj[key[p]]=value[p];
+// p++;
